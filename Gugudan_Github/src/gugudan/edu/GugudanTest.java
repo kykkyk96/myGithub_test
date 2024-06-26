1656a21cd2fad4407a63ae69ec0bb66022a986e6
kyk <beastium96@naver.com>
2024-06-26 17:41:54 +0900

    깃허브 구구단 올리기 테스트

---
+++ Gugudan_Github/src/gugudan/edu/GugudanTest.java
@@ -0,0 +1,12 @@
+package gugudan.edu;
+
+public class GugudanTest {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 9; i++) {
+			System.out.println(2 + "*" + i + "=" + 2 * i);
+		}
+	}
+
+}
